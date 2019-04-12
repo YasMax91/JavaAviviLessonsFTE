a487dbecff835fec68b2f77012d396520022ac8c
YasMax91 <m.yastremskyj@gmail.com>
2019-04-13 00:39:34 +0300

    add

---
+++ Full-time_education_Home_Task_1.1/src/HT1.java
@@ -0,0 +1,26 @@
+public class HT1 {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание №1.1");
+        System.out.print("\n");
+        System.out.println("*****************");
+        System.out.println("* ПРЯМОУГОЛЬНИК *");
+        System.out.println("*****************");
+        System.out.println(" ________ ");
+        System.out.println("|        |");
+        System.out.println(" ¯¯¯¯¯¯¯¯");
+        System.out.print("\n");
+        System.out.println("***************");
+        System.out.println("* ТРЕУГОЛЬНИК *");
+        System.out.println("***************");
+        System.out.println("     /|");
+        System.out.println("    / |");
+        System.out.println("   /  |");
+        System.out.println("  /   |");
+        System.out.println(" /    |");
+        System.out.println("/     |");
+        System.out.println("¯¯¯¯¯¯");
+        System.out.print("\n");
+        System.out.println("Домашнее задание №1.1 окончено");
+
+    }
+}
