5b9a1a8a57c12ef6041c0071af450e3b5d2e8424
YasMax91 <m.yastremskyj@gmail.com>
2019-04-13 00:30:18 +0300

    add

---
+++ Full-time_education_Home_Task_1/src/HT1.java
@@ -0,0 +1,38 @@
+public class HT1 {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание №1");
+        byte KB = 100;
+        short MB = 785;
+        int GB = 1485;
+        long TB = -4543;
+        float PB = 98546f;
+        double EB = -1568.58d;
+        char Symbol = '0';
+        boolean isTrue = true;
+        System.out.print("\n");
+        System.out.println("Переменная с наименьшим числом = " + TB);
+        System.out.println("Переменная с наибольшим числом = " + PB);
+        System.out.print("\n");
+        System.out.println("Сортировка чисел по возростанию ↑");
+        System.out.println("1.long = " + TB);
+        System.out.println("2.double = " + EB);
+        System.out.println("3.char = " + Symbol);
+        System.out.println("4.boolean = " + isTrue);
+        System.out.println("5.byte = " + KB);
+        System.out.println("6.short = " + MB);
+        System.out.println("7.int = " + GB);
+        System.out.println("8.float = " + PB);
+        System.out.print("\n");
+        System.out.println("Сортировка чисел по убыванию ↓");
+        System.out.println("8.float = " + PB);
+        System.out.println("7.int = " + GB);
+        System.out.println("6.short = " + MB);
+        System.out.println("5.byte = " + KB);
+        System.out.println("4.boolean = " + isTrue);
+        System.out.println("3.char = " + Symbol);
+        System.out.println("2.double = " + EB);
+        System.out.println("1.long = " + TB);
+        System.out.print("\n");
+        System.out.println("Домашнее задание №1 окончено");
+    }
+}
